1ea5771e0ecd8a8b216a49bc8d8e51377746f641
Temirbolat <snake2009003@mail.ru>
2023-05-18 21:16:56 +0600

    Added 'IsCustomAdminUser' permission to validate whether the property is_deleted sent to admin or not

--- apps/auths/permissions.py
+++ apps/auths/permissions.py
@@ -5,10 +5,33 @@ from rest_framework.request import Request as DRF_Request
 
 
 class IsNonDeletedUser(BasePermission):
-    def has_permission(self, request: DRF_Request, view: Any):
+    def has_permission(self, request: DRF_Request, view: Any) -> bool:
         """Handle request permissions."""
         return bool(
             request.user and
             request.user.is_authenticated and
             not request.user.datetime_deleted
         )
+
+
+class IsCustomAdminUser(BasePermission):
+    """IsCustomAdminUser."""
+
+    message: str = "Вы не админ, чтобы запрашивать удалённых пользователей."
+
+    def is_user_active(self, request: DRF_Request) -> bool:
+        """Get is user active or not."""
+        return bool(
+            request.user and
+            request.user.is_authenticated and
+            request.user.is_active
+        )
+
+    def has_permission(self, request: DRF_Request, view: Any) -> bool:
+        is_deleted: bool = bool(request.query_params.get("is_deleted", False))
+        is_admin: bool = request.user.is_superuser
+        if is_deleted and not is_admin and not self.is_user_active(
+            request=request
+        ):
+            return False
+        return True
