e151bf21b65438752e2a9d853994cf88733226dc
Temirbolat <snake2009003@mail.ru>
2023-06-15 12:34:19 +0600

    Added signal import

--- apps/auths/apps.py
+++ apps/auths/apps.py
@@ -5,3 +5,6 @@ class AuthsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'auths'
     verbose_name: str = "Авторизация"
+
+    def ready(self) -> None:
+        import auths.signals  # noqa
