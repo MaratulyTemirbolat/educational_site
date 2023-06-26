7358f47161d7919ed43744bf04ba424d40cfcda9
Temirbolat <snake2009003@mail.ru>
2023-06-26 18:38:24 +0600

    Replaced IsNonDeleted permission by AllowAny in ClassViewSet.

--- apps/subjectss/views.py
+++ apps/subjectss/views.py
@@ -206,7 +206,6 @@ class ClassViewSet(ModelInstanceMixin, DRFResponseHandler, ViewSet):
     """ClassViewSet."""
 
     queryset: Manager = Class.objects
-    permission_classes: tuple[Any] = (IsNonDeletedUser,)
     pagination_class: AbstractPageNumberPaginator = AbstractPageNumberPaginator
     serializer_class: ClassBaseSerializer = ClassBaseSerializer
 
